f53521646c43e16dc700a309cc6625c4f2b4c73e
Dustin Mundy <Dustin.A.Mundy@gmail.com>
2018-03-11 12:40:55 -0700

    Adding some basic stuff and dependencies

---
+++ src/main/java/com/vergilprime/angelessentials/AngelEssentials.java
@@ -0,0 +1,10 @@
+package com.vergilprime.angelessentials;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class AngelEssentials extends JavaPlugin {
+    @Override
+    public void onEnable(){
+        ///
+    }
+}
